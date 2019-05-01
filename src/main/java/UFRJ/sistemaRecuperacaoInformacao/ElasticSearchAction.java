10d2df9487d4a2883c77cb1e968a68014a5b5bfa
Victor Pedro <vlisboa@ufrj.br>
2019-05-01 16:59:32 -0300

    fazendo o metodo responsavel por popular os indices com todos os elementos do array.

--- src/main/java/UFRJ/sistemaRecuperacaoInformacao/ElasticSearchAction.java
+++ src/main/java/UFRJ/sistemaRecuperacaoInformacao/ElasticSearchAction.java
@@ -20,83 +20,66 @@ import org.elasticsearch.script.mustache.SearchTemplateRequestBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ElasticSearchAction
 {
 	private Settings settings;
-	
+
 	private TransportClient client;
-	
-	public ElasticSearchAction() throws UnknownHostException
-	{
-		settings = Settings.builder()
-		        .put("cluster.name", "docker-cluster").build();
-		
+
+	public ElasticSearchAction() throws UnknownHostException {
+		settings = Settings.builder().put("cluster.name", "docker-cluster").build();
+
 		client = new PreBuiltTransportClient(settings)
-		        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
-		
+				.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
 	}
-	
+
 	public void adicionarIndice(JSONArray ja) throws IOException
 	{
-		
-		client.prepareIndex("documentos", "documento",ja.getJSONObject(1).get("DOCID").toString())
-		        .setSource(jsonBuilder()
-		                    .startObject()
-		                        .field("DOCID", ja.getJSONObject(1).get("DOCID"))
-		                        .field("DOCNO", ja.getJSONObject(1).get("DOCNO"))
-		                        .field("DATE", ja.getJSONObject(1).get("DATE"))
-		                        .field("TEXT", ja.getJSONObject(1).get("TEXT"))
-		                    .endObject()
-		                  )
-		        .get();
+		for (int i = 0; i < ja.length(); i++)
+		{
+			client.prepareIndex("documentos", "documento", String.valueOf(i))
+					.setSource(jsonBuilder().startObject().field("DOCID", ja.getJSONObject(i).get("DOCID"))
+							.field("DOCNO", ja.getJSONObject(i).get("DOCNO"))
+							.field("DATE", ja.getJSONObject(i).get("DATE"))
+							.field("TEXT", ja.getJSONObject(i).get("TEXT")).endObject())
+					.get();
+		}
 	}
-	
-	public void atualizarIndice(JSONArray ja) throws IOException, InterruptedException, ExecutionException
+
+	public void atualizarIndice(JSONObject ja, String id) throws IOException, InterruptedException, ExecutionException
 	{
 
 		UpdateRequest updateRequest = new UpdateRequest();
 		updateRequest.index("documento");
 		updateRequest.type("3");
-		updateRequest.id("AWp0ei-j-NePops-Ed__");
-		updateRequest.doc(jsonBuilder()
-                .startObject()
-                    .field("DOCID", ja.getJSONObject(1).get("DOCID"))
-                    .field("DOCNO", ja.getJSONObject(1).get("DOCNO"))
-                    .field("DATE", ja.getJSONObject(1).get("DATE"))
-                    .field("TEXT", ja.getJSONObject(1).get("TEXT"))
-                .endObject()
-              );
+		updateRequest.id(id);
+		updateRequest.doc(jsonBuilder().startObject()
+				.field("DOCID", ja.get("DOCID"))
+				.field("DOCNO", ja.get("DOCNO"))
+				.field("DATE", ja.get("DATE"))
+				.field("TEXT", ja.get("TEXT")).endObject());
 		client.update(updateRequest).get();
 	}
-	
+
 	public void deletarIndice(String indice) throws UnknownHostException
 	{
 		client.prepareDelete("documento", "3", indice).get();
 	}
-	
+
 	public void realizarConsulta()
 	{
 		Map<String, Object> template_params = new HashMap<>();
 		template_params.put("param_gender", "male");
-		
+
 		SearchResponse sr = new SearchTemplateRequestBuilder(client)
-		        .setScript("{\n" + 
-		        		"    \"query\": {\n" + 
-		        		"        \"multi_match\" : {\n" + 
-		        		"            \"query\" : \"ano\",\n" + 
-		        		"            \"fields\": [\"DATE\",\"TEXT\"],\n" + 
-		        		"            \"fuzziness\": \"AUTO\"\n" + 
-		        		"        }\n" + 
-		        		"    },\n" + 
-		        		"    \"_source\": [\"DOCID\",\"DOCNO\",\"DATE\",\"TEXT\"],\n" + 
-		        		"    \"size\": 100\n" + 
-		        		"}")
-		        .setScriptType(ScriptType.INLINE)    
-		        .setRequest(new SearchRequest())                   
-		        .get()                                             
-		        .getResponse();
-		
+				.setScript("{\n" + "    \"query\": {\n" + "        \"multi_match\" : {\n"
+						+ "            \"query\" : \"ano\",\n" + "            \"fields\": [\"TEXT\"],\n"
+						+ "            \"fuzziness\": \"AUTO\"\n" + "        }\n" + "    },\n"
+						+ "    \"_source\": [\"DOCID\",\"DOCNO\",\"DATE\",\"TEXT\"],\n" + "    \"size\": 100\n" + "}")
+				.setScriptType(ScriptType.INLINE).setRequest(new SearchRequest()).get().getResponse();
+
 		SearchHit[] hits = sr.getHits().getHits();
 		System.out.println(hits[0].getScore());
 		System.out.println(hits[0].getSource().get("DOCID"));
